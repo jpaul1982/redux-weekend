d2c6f8160a62f44d67a49e2baf639f86e854b0b8
Paul Molnar <saintpaul55012@yahoo.com>
2019-06-15 14:07:23 -0500

    post route built and tested via postman

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const pool = require('./modules/pool')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -9,7 +10,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.post('/', (req, res) => {
+    const feedback = req.body;
+    pool.query(`INSERT INTO "feedback" ("feeling", "understanding", "support","comments")
+     VALUES ($1, $2, $3 ,$4);`,
+    [feedback.feeling, feedback.understanding, feedback.supported,feedback.comments])
+    .then(() => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error with INSERT feedback query', error);
+        res.sendStatus(500);
+    });
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 // handleClick - this will send the post request to server 
 // with all data collected from review inputs. 
 
 
-class Review extends Component {
-    render() {
-        return (
-            <>
-                <h1>Review Your Feedback</h1>
-                <button onClick={this.handleClick}>Submit</button>
-                <ul>
-                    <li>Feelings: {this.props.reduxState.feedbackReducer.feeling}</li>
-                    <li>Understanding: {this.props.reduxState.feedbackReducer.understanding}</li>
-                    <li>Support: {this.props.reduxState.feedbackReducer.supported}</li>
-                    <li>Comments: {this.props.reduxState.feedbackReducer.comments}</li>
-                </ul>
 
-            </>
-        )
-    }
+class Review extends Component {
+
+    handleClick = () => {
+        this.props.history.push('/thanks');
+        axios({
+            method: 'POST',
+            url: '/api/order',
+            data: this.props.reduxState.feedbackReducer
+        }).then((response) => {
+            console.log(response)
+        })
+
+    
+}
+
+render() {
+    return (
+        <>
+            <h1>Review Your Feedback</h1>
+            <button onClick={this.handleClick}>Submit</button>
+            <ul>
+                <li>Feelings: {this.props.reduxState.feedbackReducer.feeling}</li>
+                <li>Understanding: {this.props.reduxState.feedbackReducer.understanding}</li>
+                <li>Support: {this.props.reduxState.feedbackReducer.supported}</li>
+                <li>Comments: {this.props.reduxState.feedbackReducer.comments}</li>
+            </ul>
+
+        </>
+    )
+}
 }
 
 const mapReduxStateToProps = (reduxState) => ({
